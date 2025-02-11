9edc1e6f1ce0cf2e6afdbe10336db2e476deda12
qinss.pku@gmail.com <qinss.pku@gmail.com>
2025-02-11 09:27:38 -0500

    colored QQ plot of datasets

--- nat_neuro_distr.R
+++ nat_neuro_distr.R
@@ -5,8 +5,6 @@ library("viridis")
 library("ggpubr")
 
 # load the data
-AD_file_path <- "./data/Filter_AD_Paranode.xlsx"
-# Get sheet names
 
 # define the function to read the data
 read_data <- function(AD_file_path, target_column){
@@ -30,9 +28,127 @@ read_data <- function(AD_file_path, target_column){
 }
 
 ### READ TWO DATASETS
-target_column <- "Volume (px³)"
-
-
-# 
+AD_file_path <- "./data/Filter_AD_Paranode.xlsx"
 ctr_file_path <- "./data/Fitler_Ctrl_Paranode.xlsx"
-ctr_data <- read_excel(file_path, sheet = "Fig7G_area continue")
+
+# Get sheet names
+target_column <- "Volume (px³)"
+AD_data <- read_data(AD_file_path,target_column)
+ctr_data <- read_data(ctr_file_path,target_column)
+
+### COMPARE THE DISTRIBUTION OF SPHEROIDS SIZE
+# df_volume <- data.frame(AD = AD_data, Control = ctr_data)
+combined_volume <- c(AD_data, ctr_data)
+combined_labels <- c(rep("AD", length(AD_data)), rep("Control", length(ctr_data)))
+df <- data.frame(
+  volume = combined_volume,
+  source = factor(combined_labels)  # Convert to a factor
+)
+print(df)
+
+# ECDF
+g1<-ggplot(df, aes(x = volume, color = source)) +
+  stat_ecdf(geom = "step", linewidth = 1.2) +
+  labs(
+    title = "ECDF of AD and Ctr",
+    x = "Value",
+    y = "ECDF",
+    color = "Source"
+  ) +
+  theme_minimal()
+g1
+
+# PROBABILITY DENSITY
+g2 <- ggplot(df, aes(x = volume, color = source)) +
+  geom_density() +
+  labs(title = "Probability Density",
+       x = "Volune", y = "Density", color = "Condition") +
+  theme_minimal()
+g2
+
+### Q-Q plot to compare the distribution
+my.theme = list(theme(axis.line.y.left = element_line(linewidth = 0.5, color = "black"),
+                      axis.line.y.right = element_line(linewidth = 0.5, color = "black"),
+                      axis.line.x.top =  element_line(linewidth = 0.5, color ="black"),
+                      axis.line.x.bottom = element_line(linewidth = 0.5, color = "black")))
+
+qq.out <- qqplot(x=ctr_data, y=AD_data, plot.it=FALSE)
+qq.out <- as.data.frame(qq.out)
+
+names(qq.out) <- c("Control", "AD")  # Add batch names to output
+# Set the x and y limits
+xylim <- range( c(qq.out$Control, qq.out$AD) )
+
+gp <- ggscatter(qq.out, x = "Control", y = "AD",
+                size = 1.5, alpha = 0.5
+                #add = "reg.line",                                 # Add regression line
+                #conf.int = TRUE,                                  # Add confidence interval
+) + xlim(0, 250) + ylim(0, 250) + coord_fixed() +
+  geom_abline(intercept = 0, slope = 1) + 
+  geom_hline(yintercept = 10,linetype = "dashed", size = 0.2) + 
+  geom_vline(xintercept = 10,linetype = "dashed", size = 0.2) +
+  font("xlab", size = 12) + font("ylab", size = 12) + 
+  my.theme
+# ggsave("qqplot_mTOR_5XF.pdf",dpi = 600, width = 3.5, height = 3.5)
+
+
+# Using fewer data point to avoid cluter
+
+# n = max(length(AD_data), length(ctr_data))
+# p = (1:n - 1)/(n - 1)
+p = (1:1000)/1000
+q_c = quantile(ctr_data,p,na.rm=TRUE)
+q_AD = quantile(AD_data,p,na.rm=TRUE)
+qs = data.frame(Control = q_c, AD = q_AD, Quantile = p*100)
+#plot(q_m, q_f)
+new_qq <- ggplot(qs,aes(x = Control, y= AD, colour = Quantile)) + geom_point(size = 1.5) + 
+  coord_fixed() +
+  xlim(0, 250) + ylim(0, 250)  +
+  scale_color_viridis(option = "D") +
+  # scale_fill_distiller(palette = "Spectral") + 
+  geom_abline(intercept = 0, slope = 1, size = 0.5, colour = "gray") +
+  geom_hline(yintercept = 10,linetype = "dashed", size = 0.2) +
+  geom_vline(xintercept = 10,linetype = "dashed", size = 0.2) +
+  font("xlab", size = 12) + font("ylab", size = 12) +
+  theme_classic() + 
+  theme(axis.title.y = element_text(size = 12),
+        axis.title.x = element_text(size = 12),
+        axis.text = element_text(size = 10),
+        legend.position = "top")
+# new_qq
+# save as a pdf
+ggsave("nat_neuro_qq_plot_colored.pdf",dpi = 300, width = 4, height = 3.5)
+
+
+
+
+
+### Logarithm the data and compare
+df_log <- data.frame(
+    volume = log10(combined_volume),
+    source = factor(combined_labels)  # Convert to a factor
+  )
+print(df_log)
+
+g3 <- ggplot(df_log, aes(x = volume, color = source)) +
+  geom_density() +
+  labs(title = "Probability Density",
+       x = "Volune", y = "Density", color = "Condition") +
+  theme_minimal()
+g3
+
+
+pdf_plot <- ggplot() +
+  # Individual mTOR experiment curves (light gray)
+  geom_density(data = df_log, aes(x = volume, color = source),  linewidth = 1) +
+  xlab("log10(Size)") + ylab("pdf") + 
+  theme_pubr() +
+  theme(
+    legend.position = "right",
+    plot.title = element_text(hjust = 0.5, face = "bold", size = 14),
+    axis.title = element_text(size = 12),
+    axis.text = element_text(size = 10)
+  ) + 
+  scale_color_manual(values=c("#E69F00", "#56B4E9"))
+# pdf_plot
+ggsave("nat_neuro_lognorma_pdf.pdf",dpi = 600, width = 4, height = 3)
